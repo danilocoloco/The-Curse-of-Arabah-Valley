273c6dfc894046d04aa04b1d6324ce7d9ecbe0f2
danilocoloco <drodra06@estudiantes.unileon.es>
2017-06-07 19:16:55 +0200

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0F; //Velocidad de movimiento
+    public float rotationSpeed = 100.0F; //Velocidad de rotación
+
+    void Update()
+    {
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+    }
+}
